7228388b61528d0f15fe1a267b40d0213c9504e3
bansheerubber <gary.cactus.email@gmail.com>
2020-08-30 13:58:00 -0700

    fix strings being matched as operators

--- test.cs
+++ test.cs
@@ -1,3 +1,1 @@
-centerPrint(%this, "<br><br>\c6" @ %itemName @ " costs \c3" @ %price SPC $MD::CurrencyName @ "\c6.", 1);
-
-centerPrint(%this, $"<br><br>\c6{%itemName}costs \c3{%price} {$MD::CurrencyName}\c6.", 1);
+%line = strReplace(%line, ",", "");
