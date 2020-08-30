9c748ac1ce4aabbacca3b70590c686bca5abcb84
bansheerubber <gary.cactus.email@gmail.com>
2020-08-30 12:12:47 -0700

    added argument caching

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from cache import add_to_cache, has_been_modified, load_cache, save_cache
+from cache import add_cache_option, add_to_cache, has_been_modified, load_cache, save_cache
 from config import add_exported_lines, add_read_file, add_skipped_file, get_config, set_config
 from file import File
+import traceback
 from tokenizer import Tokenizer
 from time import time
 from script_file import ScriptFile
@@ -24,8 +25,7 @@ def print_help():
 		"-c, --include-cs							Include .cs files in addition to .egg files",
 		"-v, --verbose 								Verbose output",
 		"--no-comments								Do not output comments",
-		"--no-warnings								Do not print warnings",
-		"--auto-confirm								Ignore warning prompts provided by the transpiler (file overwriting warnings, etc)"
+		"--clear-cache								Ignore caching and force re-transpilation",
 	]
 	print("\n".join(help_list))
 
@@ -79,58 +79,72 @@ def transpile_file(filename, output_directory="./", file_replacement="{}.cs"):
 		
 		add_skipped_file()
 
-optionlist, args = getopt.getopt(sys.argv[1:], "hmcvo:f:", ["help", "minify", "no-comments", "output=", "file-replace=", "include-cs", "verbose"])
+try:
+	optionlist, args = getopt.getopt(sys.argv[1:], "hmcvo:f:", ["help", "minify", "no-comments", "output=", "file-replace=", "include-cs", "verbose", "clear-cache"])
 
-if len(args) > 0:
-	set_config("output", "./")
-	set_config("filereplace", "{}.cs")
-	set_config("includecs", False)
-	
-	for option, argument in optionlist:
-		if option == "-h" or option == "--help":
-			print_help()
-		elif option == "-m" or option == "--minify":
-			set_config("minify", True)
-			set_config("nocomments", True)
-		elif option == "-o" or option == "--output":
-			set_config("output", argument)
-		elif option == "-f" or option == "--file-replace":
-			set_config("filereplace", argument)
-		elif option == "-c" or option == "--include-cs":
-			set_config("includecs", True)
-		elif option == "-v" or option == "--verbose":
-			set_config("verbose", True)
-		elif option == "--no-comments":
-			set_config("nocomments", True)
+	if len(args) > 0:
+		set_config("output", "./")
+		set_config("filereplace", "{}.cs")
+		set_config("includecs", False)
+		
+		for option, argument in optionlist:
+			if option == "-h" or option == "--help":
+				print_help()
+			elif option == "-m" or option == "--minify":
+				set_config("minify", True)
+				set_config("nocomments", True)
+				add_cache_option("minify")
+				add_cache_option("nocomments")
+			elif option == "-o" or option == "--output":
+				set_config("output", argument)
+			elif option == "-f" or option == "--file-replace":
+				set_config("filereplace", argument)
+				add_cache_option("filereplace")
+			elif option == "-c" or option == "--include-cs":
+				set_config("includecs", True)
+			elif option == "-v" or option == "--verbose":
+				set_config("verbose", True)
+			elif option == "--clear-cache":
+				set_config("force", True)
+			elif option == "--no-comments":
+				set_config("nocomments", True)
+				add_cache_option("nocomments")
 
-	# go through args and figure out what to do with them
-	for arg in args:
-		if os.path.exists(arg):
-			start = time()
-			
-			load_cache()
+		# go through args and figure out what to do with them
+		for arg in args:
+			if os.path.exists(arg):
+				start = time()
+				
+				load_cache()
 
-			if os.path.isdir(arg):
-				scan_directory(arg, output_directory=get_config("output"), file_replacement=get_config("filereplace"))
+				if os.path.isdir(arg):
+					scan_directory(arg, output_directory=get_config("output"), file_replacement=get_config("filereplace"))
+				else:
+					transpile_file(arg, output_directory=get_config("output"), file_replacement=get_config("filereplace"))
+				
+				parsed_lines = get_config("parsedlines")
+				exported_lines = get_config("exportedlines")
+				number_of_files = get_config("readfiles")
+				number_of_skipped_files = get_config("skippedfiles")
+				time_taken = "{:.2f}".format(round(time() - start, 2))
+
+				save_cache()
+
+				print(f"Read {number_of_files} script files, parsed {parsed_lines} lines and exported {exported_lines} lines in {time_taken} seconds")
+
+				if number_of_skipped_files != 0:
+					print(f"Skipped {number_of_skipped_files} unmodified script files")
 			else:
-				transpile_file(arg, output_directory=get_config("output"), file_replacement=get_config("filereplace"))
-			
-			parsed_lines = get_config("parsedlines")
-			exported_lines = get_config("exportedlines")
-			number_of_files = get_config("readfiles")
-			number_of_skipped_files = get_config("skippedfiles")
-			time_taken = "{:.2f}".format(round(time() - start, 2))
-
-			save_cache()
-
-			print(f"Read {number_of_files} files, parsed {parsed_lines} lines and exported {exported_lines} lines in {time_taken} seconds")
-
-			if number_of_skipped_files != 0:
-				print(f"Skipped {number_of_skipped_files} unmodified script files")
-		else:
-			if "-" in arg:
-				print(f"Failed to read file or directory '{arg}' (are options before files and directories? eggscript [options] [files or directory])")
-			else:
-				print(f"Failed to read file or directory '{arg}'")
-else:
-	print_help()
+				if "-" in arg:
+					print(f"Failed to read file or directory '{arg}' (are options before files and directories? eggscript [options] [files or directory])")
+				else:
+					print(f"Failed to read file or directory '{arg}'")
+	else:
+		print_help()
+except Exception as error:
+	arguments = " ".join(sys.argv[1:])
+	if "-v" in arguments or "--verbose" in arguments:
+		traceback.print_stack()
+		print(f"Encountered exception '{error.__str__()}'")
+	else:
+		print_help()
